d8a08978eaa85bb2cc1e767323d9932ecc231568
k3izozzz32 <joakofuentes2009@gmail.com>
2025-01-11 11:02:36 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.like-button').addEventListener('click', function() {
+    alert('¡Te gusta esta publicación!');
+});
+
+document.querySelector('.comment-button').addEventListener('click', function() {
+    alert('¡Comentario realizado!');
+});
